1209b2621c0a75d81610a6906642db48e53a8533
rafael.souza <rafael.souza@NTB-FM0Q813>
2022-06-06 10:50:29 -0300

    Add project files.

---
+++ DesafiadosOpRelacionais/Program.cs
@@ -0,0 +1,26 @@
+﻿double nota;
+bool notaApr, notaRecu;
+
+Console.WriteLine("Digite sua nota:");
+
+nota = double.Parse(Console.ReadLine());
+notaApr = nota >= 7;
+notaRecu = nota > 5 && nota < 7;
+
+if (notaApr)
+    Console.WriteLine($"Você foi aprovado com a nota de {nota}");
+
+else if (notaRecu)
+    Console.WriteLine($"Você tirou {nota} e está de recuperação");
+
+else
+    Console.WriteLine($"Você foi reprovado.");
+
+
+// OPERADOR TERNÁRIO
+
+Console.WriteLine("Digite sua nota:");
+double TNota = double.Parse(Console.ReadLine());
+
+var TNotaApr = TNota >= 7 ? "Você foi aprovado." : ((TNota >=5 && TNota<7 ? "Você está de recuperação." : "Você foi reprovado."));
+Console.WriteLine(TNotaApr); 
